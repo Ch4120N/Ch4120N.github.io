a50e92e7b6d78f572a8f557e0a2c8429521c5814
Ch4120N <Ch4120N@Proton.me>
2026-06-29 15:29:47 +0430

    NouseMove listener

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -3,5 +3,17 @@
     const outline = document.querySelector('.cursor-outline');
 
     if (!dot || !outline) return;
+
+    let mouseX = 0, mouseY = 0;
+    let outlineX = 0, outlineY = 0;
+
+    document.addEventListener('mousemove', (e) => {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+        
+        dot.style.left = `${mouseX}px`;
+        dot.style.top = `${mouseY}px`;
+    });
+
     
 })();
