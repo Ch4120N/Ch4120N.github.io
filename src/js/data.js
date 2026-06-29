8cb4768ac4feef1d68074b89500c895a30fa4bc5
Ch4120N <Ch4120N@Proton.me>
2026-06-29 15:24:34 +0430

    skills command

--- src/js/data.js
+++ src/js/data.js
@@ -9,5 +9,14 @@ const RESUME_DATA = {
 
 Ch4120N - A passionate security-driven technologist specializing in ethical hacking, penetration testing, and building robust cybersecurity solutions. Dedicated to privacy, automation, and contributing to the open-source security community.
     `,
-    
+    skills: {
+        "💻 Programming Languages": ["Python", "C", "C++", "C#", "Go", "Java", "JavaScript", "TypeScript", "PHP", "Ruby", "Rust", "Lua", "Perl", "Bash", "PowerShell", "HTML", "CSS", "Markdown", "Regex"],
+        "🧩 Frameworks": ["React", "NextJS", "NodeJS", "Django", "Flask", "Laravel", "jQuery", "Bootstrap", "TailwindCSS", "MaterialUI", ".NET", "CMake", "QT"],
+        "🛡️ Cyber Security": ["Kali Linux", "Burp Suite", "CHFI", "CPENT", "CEH", "Selenium"],
+        "🗄️ Databases": ["MySQL", "PostgreSQL", "MongoDB", "SQLite"],
+        "⚙️ DevOps & Cloud": ["Docker", "Git", "GitHub", "GitLab", "GitHub Actions", "AWS", "Cloudflare", "Vercel", "Nginx", "NPM", "Anaconda"],
+        "🧠 AI & Data Science": ["TensorFlow", "PyTorch"],
+        "🐧 Operating Systems": ["Linux", "Ubuntu", "Debian", "Arch", "Mint", "Red Hat", "Apple", "Windows", "BSD", "OpenBSD", "Raspberry Pi"]
+    },
+      
 };
