ab265f4e463d389394ce0998532f048ad5c14e2d
Ch4120N <Ch4120N@Proton.me>
2026-06-29 15:40:39 +0430

    add escapeHtml function

--- src/js/terminal.js
+++ src/js/terminal.js
@@ -7,5 +7,9 @@
     let commandHistory = [];
     let historyIndex = -1;
 
+    function escapeHtml(text) {
+        const map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;' };
+        return text.replace(/[&<>"']/g, m => map[m]);
+    }
     
 })();
